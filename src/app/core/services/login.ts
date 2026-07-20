a63ae20240a738093201c823980a8aae05e7330b
Ahmed Sami <ahnurmed16@gmail.com>
2026-07-20 13:23:57 +0300

    feat: implement login functionality with form validation and UI updates

--- src/app/core/services/login.ts
+++ src/app/core/services/login.ts
@@ -1,6 +1,16 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { LoginInterface, LoginResponse, UserCredentials } from '../interfaces/login';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
-export class Login {}
+export class LoginService {
+  constructor(private http:HttpClient) {}
+
+  SubmitLogin(LoginInterface:LoginInterface):Observable<any> {
+    return this.http.post(`https://egxpress.me/IconnectSecure/IntegraOntrackService.svc/GetIConnectLogin`, LoginInterface );
+  }
+
+}
